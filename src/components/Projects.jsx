5a8e46c1a317a86ae6f91ac89c4460423fd84015
Mihirrr <67405160+MihirrrSharma@users.noreply.github.com>
2023-05-28 00:08:06 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -43,7 +43,7 @@ const Projects = () => {
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Projects
           </p>
-          <p className="py-6">Check out some of the work of my portfolio</p>
+          <p className="py-6">Check out some of my work</p>
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 pb-21 sm:px-0">
